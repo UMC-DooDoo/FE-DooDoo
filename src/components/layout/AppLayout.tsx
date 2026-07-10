2365d2a60476f55aeaa6e5fe1fcee0168e62f63e
yeop1108 <yeop1108@naver.com>
2026-07-10 11:49:43 +0900

    rename: 네비게이션 바 이름 변경

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,14 +1,14 @@
 import { Outlet } from "react-router-dom";
-import BottomNav from "./BottomNav";
+import NavigationBar from "./NavigationBar";
 
 function AppLayout() {
   return (
-    <div className="flex min-h-dvh justify-center bg-[var(--neutral-100)]">
-      <div className="flex min-h-dvh w-full max-w-[430px] flex-col bg-[var(--color-bg)]">
+    <div className="flex min-h-dvh justify-center bg-neutral-100">
+      <div className="bg-bg flex min-h-dvh w-full max-w-[430px] flex-col">
         <main className="relative flex flex-1 flex-col">
           <Outlet />
         </main>
-        <BottomNav />
+        <NavigationBar />
       </div>
     </div>
   );
